a17444377c25aa602ed743d68e2c46d0cf30ca66
Jatinder Bimra <jatinder.bimra@gmail.com>
2015-11-18 14:53:22 +0530

    updated file with new changes

--- js/vendor/HighCharts/themes/benchmark-colorful.js
+++ js/vendor/HighCharts/themes/benchmark-colorful.js
@@ -1,3 +1,5 @@
+
+
 /**
  * Grid-light theme for Highcharts JS
  * @author Torstein Honsi
@@ -11,7 +13,14 @@ Highcharts.createElement('link', {
 }, null, document.getElementsByTagName('head')[0]);
 
 Highcharts.theme = {
-	colors: ["#b10034", "#a73f72", "#ffc000", "#9aaf33", "#bcd07e", "#766a62", "#c0b9b4", "#0096cc", "#814c3a", "#8c9192", "#804f9c", "#c3d600"],
+	//Previous color scheme
+	//colors: ["#b10034", "#a73f72", "#ffc000", "#9aaf33", "#bcd07e", "#766a62", "#c0b9b4", "#0096cc", "#814c3a", "#8c9192", "#804f9c", "#c3d600"],
+	//New color scheme - dated 16-Nov-15
+	//colors: ["#ff8400", "#de6148", "#ffd950", "#4ce087", "#4cb1e0", "#a0afbd", "#c96a04", "#ad3f29", "#a5871b", "#249852", "#318ab3", "#606b75"],
+	//New colors for bar's
+	//Green border color: #66cc66; bg color: #ccffb3
+	//Blue border color: #33ccff; bg color: #dbfbff
+	//Grey border color: #cccccc; bg color: #e9e9e9
 	chart: {
 		alignTicks: false,
 		backgroundColor: "#fff",
@@ -31,7 +40,7 @@ Highcharts.theme = {
         align: 'center',
         x: 0,
         y: 10,
-        margin: 10
+        margin: 20
 	},
 	subtitle : {
 		style: {
@@ -51,9 +60,9 @@ Highcharts.theme = {
 		formatter: function () {
 			if(this.x.name != '' && this.x.name != ' '){
 				return '<b>' + this.x + '</b><br/>' +
-                this.series.name + ': ' + Highcharts.numberFormat(this.y,2, '.','');	
+                this.series.name + ': ' + Highcharts.numberFormat(this.y,0, '.','');	
 			}else{
-				return this.series.name + ': ' + Highcharts.numberFormat(this.y,2, '.','');
+				return this.series.name + ': ' + Highcharts.numberFormat(this.y,0, '.','');
 			}
             
         }
@@ -84,7 +93,8 @@ Highcharts.theme = {
 				//textTransform: 'uppercase'
 				color: '#666',
 				fontSize: '12px'				
-			}
+			},
+			margin: 20
 		},
 		labels: {
 			style: {
